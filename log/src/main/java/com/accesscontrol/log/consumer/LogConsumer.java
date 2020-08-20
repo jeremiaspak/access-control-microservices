a1137decee16e2ff098894714269840e8ba08602
Jeremias Pak <jeremiaspak@gmail.com>
2020-08-20 11:50:38 -0300

    write access log to CSV file

--- log/src/main/java/com/accesscontrol/log/consumer/LogConsumer.java
+++ log/src/main/java/com/accesscontrol/log/consumer/LogConsumer.java
@@ -1,9 +1,14 @@
 package com.accesscontrol.log.consumer;
 
 import com.accesscontrol.access.model.AccessLog;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 @Component
 public class LogConsumer {
@@ -15,5 +20,18 @@ public class LogConsumer {
         + " customer " + log.getCustomerId()
         + " door " + log.getDoorId()
     );
+    try {
+      writeToFile(log);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void writeToFile(AccessLog log) throws IOException {
+    String[] HEADERS = { "customer_id", "door_id", "is_authorized" };
+    FileWriter file = new FileWriter("log.csv");
+    try (CSVPrinter printer = new CSVPrinter(file, CSVFormat.DEFAULT.withHeader(HEADERS))) {
+      printer.printRecord(log.getCustomerId(), log.getDoorId(), log.getAuthorized());
+    }
   }
 }
